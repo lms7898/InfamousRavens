2ffd9d36b70cdc9f5de9f3a5a6deac1a4b737f83
ntancredi <nxt2715@ad.rit.edu>
2017-02-10 15:17:09 -0500

    basic enemy ai
    super basic path following, still tweaking to look normal

---
+++ Occupeasants/Assets/PacingEntity.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacingEntity : MovingEntity {
+
+	/* Animator Parameters
+	 * 
+	 * Boolean - "Walking" - State activated when the entity is walking
+	 * 
+	*/
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+
+		MoveLeft ();
+	}
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+		base.FixedUpdate ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		PacingEntityBorder border = other.gameObject.GetComponent<PacingEntityBorder> ();
+
+		if (border != null) {
+			Flip ();
+		}
+	}
+
+	public void Flip() {
+		base.Flip ();
+
+		if (move < 0) {
+			MoveRight();
+		}else{
+			MoveLeft ();
+		}
+	}
+
+
+}
---
+++ Occupeasants/Assets/PacingEntityBorder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacingEntityBorder : MonoBehaviour {
+
+	// THIS IS JUST USED TO IDENTIFY THE BORDER. NO FUNCTIONALITY HERE.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
