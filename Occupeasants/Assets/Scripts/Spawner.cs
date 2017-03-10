0950ef2aa133ef1d2c5131652758ad24eac8f014
ntancredi <nxt2715@rit.edu>
2017-03-10 13:44:49 -0500

    made some changes to the spawner

--- Occupeasants/Assets/Scripts/Spawner.cs
+++ Occupeasants/Assets/Scripts/Spawner.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
     public GameObject Enemy;
+    public int SpawnTotal;
+    private bool active;
 
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(Spawn(Enemy, 2, 3));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.activeSelf)
+        {
+            Activate();
+        }
+    }
+
+    private void Activate()
+    {
+        while (!active)
+        {
+            StartCoroutine(Spawn(Enemy, 2, SpawnTotal));
+            active = true;
+        }
+        return;
+    }
+
+    
     IEnumerator Spawn(GameObject enemy, float interval, int numEnemies)
     {
         int currentCount = 0;
